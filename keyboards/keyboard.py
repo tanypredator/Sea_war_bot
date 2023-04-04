71b9dd04357a487f4a9c42177b3591fdc11d2f94
User <tanyfilina@gmail.com>
2023-04-04 19:43:27 +0500

    added keyboard for game mode selection

--- keyboards/keyboard.py
+++ keyboards/keyboard.py
@@ -19,3 +19,22 @@ yes_no_kb_builder.row(button_yes, button_no, width=2)
 yes_no_kb = yes_no_kb_builder.as_markup(
                                 one_time_keyboard=True,
                                 resize_keyboard=True)
+
+
+# ------- Создаем клавиатуру через ReplyKeyboardBuilder -------
+
+# Создаем кнопки с ответами согласия и отказа
+button_one_sided: KeyboardButton = KeyboardButton(text=LEXICON_RU['one_sided_button'])
+button_pair_AI: KeyboardButton = KeyboardButton(text=LEXICON_RU['pair_AI_button'])
+button_pair_human: KeyboardButton = KeyboardButton(text=LEXICON_RU['pair_human_button'])
+
+# Инициализируем билдер для клавиатуры с кнопками режима игры
+game_mode_kb_builder: ReplyKeyboardBuilder = ReplyKeyboardBuilder()
+
+# Добавляем кнопки в билдер с параметром width=3
+game_mode_kb_builder.row(button_one_sided, button_pair_AI, button_pair_human, width=3)
+
+# Создаем клавиатуру с кнопками "Давай!" и "Не хочу!"
+game_mode_kb = game_mode_kb_builder.as_markup(
+                                one_time_keyboard=True,
+                                resize_keyboard=True)
