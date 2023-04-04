0388178697721c7b1e555e0dfafbe82d8539f205
User <tanyfilina@gmail.com>
2023-04-04 19:39:48 +0500

    separated in-game handlers

--- handlers/user_handlers.py
+++ handlers/user_handlers.py
@@ -2,26 +2,32 @@ from aiogram import Router
 from aiogram.filters import Command, CommandStart, Text
 from aiogram.types import Message, CallbackQuery
 
-from keyboards.keyboard import yes_no_kb
+from keyboards.keyboard import yes_no_kb, game_mode_kb
 from keyboards.keyboard_map import game_kb, rebuild_keyboard
+from keyboards.pair_AI_keyboard import pair_AI_game_kb
 from lexicon.lexicon_ru import LEXICON_RU
-from services.sea_war import create_map, shot_result
+from services.sea_war import create_map, shot_result, player_map
 from User_dict.user_dict import users
 
 router: Router = Router()
 ATTEMPTS: int = 40
+TILES_LEFT = 13
 SHIPS_LEFT: int = 7
 
 # Этот хэндлер срабатывает на команду /start
 @router.message(CommandStart())
 async def process_start_command(message: Message):
-    await message.answer(text=LEXICON_RU['/start'], reply_markup=yes_no_kb)
+    await message.answer(text=LEXICON_RU['/start'], reply_markup=game_mode_kb)
     if message.from_user.id not in users:
         users[message.from_user.id] = {'in_game': False,
-                                       'sea_map': None,
+        'game_mode': None,
+                                       'AI_map': None,
+                                       'player_map': None,
                                        'attempts': None,
                                        'ships_left': None,
                                        'hits': None,
+                                       'tiles_left': None,
+                                       'tiles': None,
                                        'total_games': 0,
                                        'wins': 0}
 
@@ -38,7 +44,7 @@ async def process_stat_command(message: Message):
 # Этот хэндлер срабатывает на команду /help
 @router.message(Command(commands=['help']))
 async def process_help_command(message: Message):
-    await message.answer(text=LEXICON_RU['/help'], reply_markup=yes_no_kb)
+    await message.answer(text=LEXICON_RU['/help'], reply_markup=game_mode_kb)
 
 
 # Этот хэндлер будет срабатывать на команду "/cancel"
@@ -49,17 +55,19 @@ async def process_cancel_command(message: Message):
                              'снова - напишите об этом')
         users[message.from_user.id]['in_game'] = False
     else:
-        await message.answer('А мы итак с вами не играем. '
-                             'Может, сыграем разок?')
+        await message.answer(text ='А мы итак с вами не играем. '
+                             'Может, сыграем разок?', reply_markup=game_mode_kb)
 
 
-# Этот хэндлер срабатывает на согласие пользователя играть в игру
-@router.message(Text(text=LEXICON_RU['yes_button']))
+# Этот хэндлер срабатывает на выбор пользователя играть в игру без своего поля
+@router.message(Text(text=LEXICON_RU['one_sided_button']))
 async def process_yes_answer(message: Message):
     await message.answer(text=LEXICON_RU['yes'], reply_markup=game_kb)
     if not users[message.from_user.id]['in_game']:
         users[message.from_user.id]['in_game'] = True
-        users[message.from_user.id]['sea_map'] = create_map()
+        users[message.from_user.id]['game_mode'] = 'one_sided'
+        users[message.from_user.id]['total_games'] += 1
+        users[message.from_user.id]['AI_map'] = create_map()
         users[message.from_user.id]['hits'] = []
         users[message.from_user.id]['attempts'] = ATTEMPTS
         users[message.from_user.id]['ships_left'] = SHIPS_LEFT
@@ -75,29 +83,22 @@ async def process_no_answer(message: Message):
     await message.answer(text=LEXICON_RU['no'])
 
 
-# Этот хэндлер срабатывает на любую из игровых кнопок
-@router.callback_query(Text(text=[str(i)+','+str(j) for i in range(1,9) for j in range(1,9)]))
-async def process_game_button(callback: CallbackQuery):
-    coords = callback.data.split(',')
-    coord_y = int(coords[0]) - 1
-    coord_x = int(coords[1]) - 1
-    users[callback.from_user.id]['attempts'] -= 1
-    play_map = users[callback.from_user.id]['sea_map']
-    hits = users[callback.from_user.id]['hits']
-    result = shot_result(play_map, hits, coord_x, coord_y)
-    if result == 'killed':
-    	users[callback.from_user.id]['ships_left'] -= 1
-    if users[callback.from_user.id]['ships_left'] == 0:
-        await callback.message.edit_text(
-        text=LEXICON_RU['user_won'],
-        reply_markup=None)
-        await callback.message.answer(text=LEXICON_RU['new_game'], reply_markup=yes_no_kb)
-    elif users[callback.from_user.id]['attempts'] == 0:
-       await callback.message.edit_text(
-        text=LEXICON_RU['user_failed'],
-       reply_markup=None)
-       await callback.message.answer(text=LEXICON_RU['new_game'], reply_markup=yes_no_kb)
+# Этот хэндлер срабатывает на выбор пользователя играть в игру против компьютера
+@router.message(Text(text=LEXICON_RU['pair_AI_button']))
+async def process_yes_answer(message: Message):
+    await message.answer(text=LEXICON_RU['yes'], reply_markup=pair_AI_game_kb)
+    if not users[message.from_user.id]['in_game']:
+        users[message.from_user.id]['in_game'] = True
+        users[message.from_user.id]['game_mode'] = 'pair_AI'
+        users[message.from_user.id]['total_games'] += 1
+        users[message.from_user.id]['AI_map'] = create_map()
+        users[message.from_user.id]['hits'] = []
+        users[message.from_user.id]['tiles'] = []
+        users[message.from_user.id]['tiles_left'] = TILES_LEFT
+        users[message.from_user.id]['ships_left'] = SHIPS_LEFT
+        users[message.from_user.id]['player_map'] = player_map()
+        users[message.from_user.id]['player_ships'] = {}
     else:
-        await callback.message.edit_text(
-        text=LEXICON_RU[result], reply_markup=rebuild_keyboard(callback.message.reply_markup.inline_keyboard, coord_x, coord_y, result))
-    await callback.answer(text = f"{LEXICON_RU['shots left']} = {users[callback.from_user.id]['attempts']}")
+        await message.answer('Пока мы играем в игру я могу '
+                             'реагировать только на нажатие кнопок на игровом поле '
+                             'и команды /cancel и /stat')
