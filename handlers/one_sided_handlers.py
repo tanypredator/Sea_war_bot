00c67344fad9bee10443d933bc4ace80597a70c7
User <tanyfilina@gmail.com>
2023-04-05 18:15:18 +0500

    improved readability

--- handlers/one_sided_handlers.py
+++ handlers/one_sided_handlers.py
@@ -5,7 +5,7 @@ from aiogram.types import Message, CallbackQuery
 from keyboards.keyboard import yes_no_kb, game_mode_kb
 from keyboards.keyboard_map import rebuild_keyboard
 from lexicon.lexicon_ru import LEXICON_RU
-from services.sea_war import create_map, shot_result
+from services.sea_war import shot_result
 from User_dict.user_dict import users
 
 router: Router = Router()
@@ -19,26 +19,27 @@ async def process_game_button(callback: CallbackQuery):
     coords = callback.data.split(',')
     coord_y = int(coords[0]) - 1
     coord_x = int(coords[1]) - 1
-    users[callback.from_user.id]['attempts'] -= 1
-    play_map = users[callback.from_user.id]['AI_map']
-    hits = users[callback.from_user.id]['hits']
-    result = shot_result(play_map, hits, coord_x, coord_y)
+    user = users[callback.from_user.id]
+    user['attempts'] -= 1
+    play_map = user['AI_map']
+    hits = user['hits']
+    result = shot_result(play_map[0], play_map[1], hits, coord_x, coord_y)
     if result == 'killed':
-    	users[callback.from_user.id]['ships_left'] -= 1
-    if users[callback.from_user.id]['ships_left'] == 0:
+    	user['ships_left'] -= 1
+    if user['ships_left'] == 0:
         await callback.message.edit_text(
         text=LEXICON_RU['user_won'],
         reply_markup=None)
-        users[callback.from_user.id]['wins'] += 1
-        users[callback.from_user.id]['in_game'] = False
-        await callback.message.answer(text=LEXICON_RU['new_game'], reply_markup=yes_no_kb)
-    elif users[callback.from_user.id]['attempts'] == 0:
+        user['wins'] += 1
+        user['in_game'] = False
+        await callback.message.answer(text=LEXICON_RU['new_game'], reply_markup=game_mode_kb)
+    elif user['attempts'] == 0:
        await callback.message.edit_text(
         text=LEXICON_RU['user_failed'],
        reply_markup=None)
-       users[callback.from_user.id]['in_game'] = False
-       await callback.message.answer(text=LEXICON_RU['new_game'], reply_markup=yes_no_kb)
+       user['in_game'] = False
+       await callback.message.answer(text=LEXICON_RU['new_game'], reply_markup=game_mode_kb)
     else:
         await callback.message.edit_text(
         text=LEXICON_RU[result], reply_markup=rebuild_keyboard(callback.message.reply_markup.inline_keyboard, coord_x, coord_y, result))
-    await callback.answer(text = f"{LEXICON_RU['shots left']} = {users[callback.from_user.id]['attempts']}")
+    await callback.answer(text = f"{LEXICON_RU['shots left']} = {user['attempts']}")
