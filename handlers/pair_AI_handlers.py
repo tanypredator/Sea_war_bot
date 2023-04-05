f1b08287a57ef744a1c74f278fd4d857fb7fce9c
User <tanyfilina@gmail.com>
2023-04-05 20:28:54 +0500

    improved confirmation handler

--- handlers/pair_AI_handlers.py
+++ handlers/pair_AI_handlers.py
@@ -3,7 +3,7 @@ from aiogram.filters import Command, CommandStart, Text
 from aiogram.types import Message, CallbackQuery
 
 from keyboards.keyboard import yes_no_kb, game_mode_kb
-from keyboards.pair_AI_keyboard import player_keyboard_rebuild, player_keyboard_restore
+from keyboards.pair_AI_keyboard import player_keyboard_rebuild, player_game_kb, player_map_restore
 from keyboards.keyboard_map import game_kb, rebuild_keyboard
 from lexicon.lexicon_ru import LEXICON_RU
 from services.sea_war import create_AI_map, shot_result, player_ship_placement
@@ -35,27 +35,35 @@ async def process_game_button(callback: CallbackQuery):
         text=f"{LEXICON_RU['tiles left']} = {user['tiles_left']}", reply_markup=player_keyboard_rebuild(callback.message.reply_markup.inline_keyboard, coord_x, coord_y, status))
     
     if user['tiles_left'] == 0:
-        await callback.message.edit_text(
+    	await callback.message.edit_text(
         text=LEXICON_RU['no_tiles_left'], reply_markup=callback.message.reply_markup)
 
     await callback.answer()
-
 
 # Этот хэндлер срабатывает на подтверждение расположения кораблей 
 @router.callback_query(Text(text='confirm_placement'))
 async def confirm_placement(callback: CallbackQuery):
     user = users[callback.from_user.id]
     result = player_ship_placement(user['player_ships'], user['player_map'])[0]
-    user['player_ships'] = player_ship_placement(user['player_ships'], user['player_map'])[0]
+    user['player_ships'] = player_ship_placement(user['player_ships'], user['player_map'])[1]
     
     if result == "ship too long":
-        await callback.message.edit_text(text=LEXICON_RU['ship_too_long'], reply_markup=player_keyboard_restore())
+        user['tiles_left'] = 13
+        user['player_map'] = player_map_restore(user['player_map'], user['tiles'])[0]
+        user['tiles'] = player_map_restore(user['player_map'], user['tiles'])[1]
+        await callback.message.edit_text(text=LEXICON_RU['ship_too_long'], reply_markup=player_game_kb)
     
     elif result == "diagonal placement":
-        await callback.message.edit_text(	text=LEXICON_RU['diagonal_placement'], reply_markup=player_keyboard_restore())
+        user['tiles_left'] = 13
+        user['player_map'] = player_map_restore(user['player_map'], user['tiles'])[0]
+        user['tiles'] = player_map_restore(user['player_map'], user['tiles'])[1]
+        await callback.message.edit_text(	text=LEXICON_RU['diagonal_placement'], reply_markup=player_game_kb)
    
     elif result == "wrong placement":
-        await callback.message.edit_text(	text=LEXICON_RU['wrong_placement'], reply_markup=player_keyboard_restore())
+        user['tiles_left'] = 13
+        user['player_map'] = player_map_restore(user['player_map'], user['tiles'])[0]
+        user['tiles'] = player_map_restore(user['player_map'], user['tiles'])[1]
+        await callback.message.edit_text(	text=LEXICON_RU['wrong_placement'], reply_markup=player_game_kb)
     
     elif result == "placement confirmed":
         await callback.message.edit_text(	text=LEXICON_RU['placement_confirmed'], reply_markup=game_kb)
