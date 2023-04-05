c4908207f699db40d0b49fbe66d71ad2af7b0513
User <tanyfilina@gmail.com>
2023-04-05 18:09:49 +0500

    deleted unused code

--- Sea_war_bot.py
+++ Sea_war_bot.py
@@ -27,29 +27,3 @@ async def main():
 
 if __name__ == '__main__':
     asyncio.run(main())
-
-
-
-'''
-# Количество попыток, доступных пользователю в игре
-ATTEMPTS: int = 40
-
-# Словарь, в котором будут храниться данные пользователя
-users: dict = {}
-
-
-# Этот хэндлер будет срабатывать на команду "/start"
-@dp.message(Command(commands=['start']))
-async def process_start_command(message: Message):
-    await message.answer('Привет!\nДавай сыграем в игру "Угадай число"?\n\n'
-                         'Чтобы получить правила игры и список доступных '
-                         'команд - отправьте команду /help')
-    # Если пользователь только запустил бота и его нет в словаре '
-    # 'users - добавляем его в словарь
-    if message.from_user.id not in users:
-        users[message.from_user.id] = {'in_game': False,
-                                       'secret_number': None,
-                                       'attempts': None,
-                                       'total_games': 0,
-                                       'wins': 0}
-'''
