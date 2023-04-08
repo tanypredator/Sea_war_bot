9a2a66e72ecaba020b67d1186d3ba0a5c7a292f1
User <tanyfilina@gmail.com>
2023-04-08 11:34:50 +0500

    fixed indentation

--- handlers/one_sided_handlers.py
+++ handlers/one_sided_handlers.py
@@ -1,8 +1,8 @@
 from aiogram import Router
-from aiogram.filters import Command, CommandStart, Text
-from aiogram.types import Message, CallbackQuery
+from aiogram.filters import Text
+from aiogram.types import CallbackQuery
 
-from keyboards.keyboard import yes_no_kb, game_mode_kb
+from keyboards.keyboard import game_mode_kb
 from keyboards.keyboard_map import rebuild_keyboard
 from lexicon.lexicon_ru import LEXICON_RU
 from services.sea_war import shot_result
@@ -14,7 +14,7 @@ SHIPS_LEFT: int = 7
 
 
 # Этот хэндлер срабатывает на любую из игровых кнопок
-@router.callback_query(Text(text=['AI,'+str(i)+','+str(j) for i in range(1,9) for j in range(1,9)]))
+@router.callback_query(Text(text=['AI,'+str(i)+','+str(j) for i in range(1, 9) for j in range(1, 9)]))
 async def process_game_button(callback: CallbackQuery):
     coords = callback.data.split(',')
     coord_y = int(coords[1])
@@ -23,13 +23,13 @@ async def process_game_button(callback: CallbackQuery):
     user['attempts'] -= 1
     play_map = user['AI_map']
     for row in play_map[0]:
-    	print(row)
+        print(row)
     hits = user['player_hits']
     print(coord_x, coord_y)
     result = shot_result(play_map[0], play_map[1], hits, coord_x, coord_y)
     print(result)
     if result == 'killed':
-    	user['AI_ships_left'] -= 1
+        user['AI_ships_left'] -= 1
     if user['AI_ships_left'] == 0:
         await callback.message.edit_text(
         text=LEXICON_RU['user_won'],
@@ -45,5 +45,6 @@ async def process_game_button(callback: CallbackQuery):
        await callback.message.answer(text=LEXICON_RU['new_game'], reply_markup=game_mode_kb)
     else:
         await callback.message.edit_text(
-        text=LEXICON_RU[result], reply_markup=rebuild_keyboard(callback.message.reply_markup.inline_keyboard, coord_x, coord_y, result))
-    await callback.answer(text = f"{LEXICON_RU['shots left']} = {user['attempts']}")
+        text=LEXICON_RU[result], reply_markup=rebuild_keyboard(callback.message.reply_markup.inline_keyboard,
+                                                               coord_x, coord_y, result))
+    await callback.answer(text=f"{LEXICON_RU['shots left']} = {user['attempts']}")
