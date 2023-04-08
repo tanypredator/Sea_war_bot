617a87213c1379e1a8f609669f2a7508d509ab2a
User <tanyfilina@gmail.com>
2023-04-08 11:34:06 +0500

    confirm_player_kb function makes tiles' buttons inactive

--- keyboards/player_map_keyboard.py
+++ keyboards/player_map_keyboard.py
@@ -2,7 +2,6 @@ from aiogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 
 from lexicon.lexicon_ru import LEXICON_RU
 
-
 # ------- Создаем игровую клавиатуру без использования билдера -------
 
 
@@ -14,72 +13,84 @@ keyboard: list[list[InlineKeyboardButton]] = []
 for i in range(1, 9):
     for j in range(1, 9):
         buttons.append(InlineKeyboardButton(
-        text = '🌊',
-        callback_data=f'{i},{j}'))
+            text='🌊',
+            callback_data=f'{i},{j}'))
         if not j % 8:
             keyboard.append(buttons)
             buttons = []
 
 confirm_buttons = []
 confirm_buttons.append(InlineKeyboardButton(
-        text = LEXICON_RU['confirm_placement'],
-        callback_data='confirm_placement'))
+    text=LEXICON_RU['confirm_placement'],
+    callback_data='confirm_placement'))
 confirm_buttons.append(InlineKeyboardButton(
-        text = LEXICON_RU['/cancel'],
-        callback_data='/cancel'))
+    text=LEXICON_RU['/cancel'],
+    callback_data='/cancel'))
 
 keyboard.append(confirm_buttons)
 
 # Создаем игровую клавиатуру с нумерованными кнопками как список списков
 
 player_game_kb: InlineKeyboardMarkup = InlineKeyboardMarkup(
-                                    inline_keyboard=keyboard,
-                                  resize_keyboard=True)
+    inline_keyboard=keyboard,
+    resize_keyboard=True)
 
 
 #  rebuild keyboard after ship deck placement
 def player_keyboard_rebuild(old_board, x, y, status):
-	keyboard = old_board
-	x -= 1
-	y -= 1
-	if status == "empty":
-		keyboard[y][x] = InlineKeyboardButton(
-        text = '🌊',
-        callback_data=f'{y},{x}')
-	elif status == "place":
-		keyboard[y][x] = InlineKeyboardButton(
-        text = '🔲',
-        callback_data=f'{y},{x}')
-	rebuilt_keyboard: InlineKeyboardMarkup = InlineKeyboardMarkup( inline_keyboard=keyboard,
-                                  resize_keyboard=True)
-	return rebuilt_keyboard
+    keyboard = old_board
+    x -= 1
+    y -= 1
+    if status == "empty":
+        keyboard[y][x] = InlineKeyboardButton(
+            text='🌊',
+            callback_data=f'{y},{x}')
+    elif status == "place":
+        keyboard[y][x] = InlineKeyboardButton(
+            text='🔲',
+            callback_data=f'{y},{x}')
+    rebuilt_keyboard: InlineKeyboardMarkup = InlineKeyboardMarkup(inline_keyboard=keyboard,
+                                                                  resize_keyboard=True)
+    return rebuilt_keyboard
 
 
 # remove previously chosen tiles from map and list of tiles
 def player_map_restore(player_map: list[list], tiles: list[list]):
-	for tile in tiles:
-		y = tile[0]
-		x = tile[1]
-		player_map[y][x] = 0
-		tiles.remove(tile)
-	return (player_map, tiles)
+    for tile in tiles:
+        y = tile[0]
+        x = tile[1]
+        player_map[y][x] = 0
+        tiles.remove(tile)
+    return (player_map, tiles)
 
 
-# change confirmation button to the next move button
-def confirm_player_kb(player_kb):
-	new_player_kb = player_kb
-	new_player_kb.pop()
-	
-	next_move_AI_button = []
-	next_move_AI_button.append(InlineKeyboardButton(
-        text = LEXICON_RU['next_move'],
-        callback_data='next_move'))
-	next_move_AI_button.append(InlineKeyboardButton(
-        text = LEXICON_RU['/cancel'],
+# change confirmation button to the next move button and make tiles inactive
+def confirm_player_kb(player_map):
+    in_game_buttons: list[InlineKeyboardButton] = []
+    in_game_keyboard: list[list[InlineKeyboardButton]] = []
+    for y in range(1, 9):
+        for x in range(1, 9):
+            if player_map[y][x] == 1:
+                in_game_buttons.append(InlineKeyboardButton(
+                    text='🔲',
+                    callback_data=f'inactive,{y},{x}'))
+            else: in_game_buttons.append(InlineKeyboardButton(
+                    text='🌊',
+                    callback_data=f'inactive,{y},{x}'))
+            if not x % 8:
+                in_game_keyboard.append(in_game_buttons)
+                in_game_buttons = []
+
+    next_move_player_button = []
+    next_move_player_button.append(InlineKeyboardButton(
+        text=LEXICON_RU['next_move'],
+        callback_data='next_move_player'))
+    next_move_player_button.append(InlineKeyboardButton(
+        text=LEXICON_RU['/cancel'],
         callback_data='/cancel'))
-	new_player_kb.append(next_move_AI_button)
-	
-	in_game_player_kb: InlineKeyboardMarkup = InlineKeyboardMarkup( inline_keyboard=new_player_kb,
-                                  resize_keyboard=True)
-    
-	return in_game_player_kb
+    in_game_keyboard.append(next_move_player_button)
+
+    in_game_player_kb: InlineKeyboardMarkup = InlineKeyboardMarkup(inline_keyboard=in_game_keyboard,
+                                                                   resize_keyboard=True)
+
+    return in_game_player_kb
