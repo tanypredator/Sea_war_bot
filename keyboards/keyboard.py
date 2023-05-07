59acdbe76143b6cd2119f5777fc7ce60db9f5912
User <tanyfilina@gmail.com>
2023-05-07 11:30:53 +0500

    invitation_kb in work

--- keyboards/keyboard.py
+++ keyboards/keyboard.py
@@ -1,4 +1,5 @@
-from aiogram.types import ReplyKeyboardMarkup, KeyboardButton, KeyboardButtonRequestUser
+from aiogram.types import ReplyKeyboardMarkup, KeyboardButton, KeyboardButtonRequestUser, InlineKeyboardButton, \
+    InlineKeyboardMarkup
 from aiogram.utils.keyboard import ReplyKeyboardBuilder
 
 from lexicon.lexicon_ru import LEXICON_RU
@@ -17,9 +18,8 @@ yes_no_kb_builder.row(button_yes, button_no, width=2)
 
 # Создаем клавиатуру с кнопками "Давай!" и "Не хочу!"
 yes_no_kb = yes_no_kb_builder.as_markup(
-                                one_time_keyboard=True,
-                                resize_keyboard=True)
-
+    one_time_keyboard=True,
+    resize_keyboard=True)
 
 # ------- Создаем клавиатуру через ReplyKeyboardBuilder -------
 
@@ -36,18 +36,40 @@ game_mode_kb_builder.row(button_one_sided, button_pair_AI, button_pair_human, wi
 
 # Создаем клавиатуру с кнопками "Давай!" и "Не хочу!"
 game_mode_kb = game_mode_kb_builder.as_markup(
-                                one_time_keyboard=True,
-                                resize_keyboard=True)
+    one_time_keyboard=True,
+    resize_keyboard=True)
 
-
+# keyboard for pair_human mode
 button_choose_enemy: KeyboardButton = KeyboardButton(
-            text="Выбрать противника",
-            request_user=KeyboardButtonRequestUser(request_id=1))
+    text="Выбрать противника",
+    request_user=KeyboardButtonRequestUser(request_id=1))
 
 choose_enemy_kb_builder: ReplyKeyboardBuilder = ReplyKeyboardBuilder()
 
 choose_enemy_kb_builder.row(button_choose_enemy)
 
 choose_enemy_kb = choose_enemy_kb_builder.as_markup(
-                                one_time_keyboard=True,
-                                resize_keyboard=True)
+    one_time_keyboard=True,
+    resize_keyboard=True)
+
+
+# invitation_kb
+def make_invitation_kb(player_id):
+    confirm_invitation_buttons: list[InlineKeyboardButton] = []
+    keyboard: list[list[InlineKeyboardButton]] = []
+
+    confirm_invitation_buttons.append(InlineKeyboardButton(
+        text=LEXICON_RU['yes_button'],
+        callback_data=f'yes,{player_id}'))
+
+    confirm_invitation_buttons.append(InlineKeyboardButton(
+        text=LEXICON_RU['no_button'],
+        callback_data=f'no,{player_id}'))
+
+    keyboard.append(confirm_invitation_buttons)
+
+    invitation_kb: InlineKeyboardMarkup = InlineKeyboardMarkup(
+        inline_keyboard=keyboard,
+        resize_keyboard=True)
+
+    return invitation_kb
